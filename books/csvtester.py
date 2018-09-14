2f6f04a79ff21dd1e0ad0c3d9ed0de9a5a2744ae
Daniel B <busisd@carleton.edu>
2018-09-14 17:05:22 -0500

    csvtester.py works for book titles now, haven't touched command line stuff yet tho

--- books/csvtester.py
+++ books/csvtester.py
@@ -1,8 +1,25 @@
 import csv
 
-unix_dialect = csv.get_dialect("unix")
-with open("books.csv", newline='') as csvfile:
-    book_reader = csv.reader(csvfile, dialect="unix")
-    for entry in book_reader:
-        print(entry[0])
+input_file = input("Input file? ")
+sort_direction = input("Sort direction? 'reverse', 'forward', or empty (defaults to forward) ")
+action = input("Action? 'books' or 'authors' ")
 
+#these inputs would normally come from the command line, using normal python input for now
+#also needs error checking and failure statement as described in instructions
+
+unix_dialect = csv.get_dialect("unix")
+with open(input_file, newline='') as csvfile:
+    book_reader = csv.reader(csvfile, dialect="unix")
+    if action == "books":
+        title_list = []
+        for entry in book_reader:
+            title_list.append(entry[0])
+        title_list.sort()
+        if sort_direction == "reverse":
+            title_list.reverse()        
+        for title in title_list:
+            print(title)
+    elif action == 'authors':
+        pass
+    else:
+        print("you didn't write 'books' or 'authors'!")
