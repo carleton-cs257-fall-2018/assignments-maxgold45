72cf03c66b1361a230683e1cfe6b08b5cc142a4f
Daniel B <busisd@carleton.edu>
2018-09-20 19:25:15 -0500

    Added some tests based on authors and books_for_author

--- books/booksdatasourcetest.py
+++ books/booksdatasourcetest.py
@@ -20,6 +20,9 @@ class BooksDataSourceTest(unittest.TestCase):
 
     def tearDown(self):
         pass
+
+    def test_data_source_exists(self):
+        self.assertTrue(self.data_source is not None)
     
     def test_book_string(self):
         self.assertRaises(TypeError, self.data_source.book, "This should not fail")
@@ -27,16 +30,14 @@ class BooksDataSourceTest(unittest.TestCase):
     def test_book_negative(self):
         self.assertRaises(ValueError, self.data_source.book, -5)
 
-    def test_book_zero(self):
-        self.assertRaises(ValueError, self.data_source.book, 0)
+##    def test_book_zero(self):
+##        self.assertRaises(ValueError, self.data_source.book, 0)
+## Since book id's start at 0, pretty sure the test_book_0 method is correct implementation
 
     def test_book_overly_large(self):
         self.assertRaises(ValueError, self.data_source.book, 9999)
         #self.assertEqual(self.data_source.book(9999), {})
     
-    def test_data_source_exists(self):
-        self.assertTrue(self.data_source is not None)
-
     def test_book_0(self):
         book_0 = {'id': 0, 'title': 'All Clear', 'publication_year': 2010}
         self.assertEqual(self.data_source.book(0), book_0)
@@ -45,9 +46,36 @@ class BooksDataSourceTest(unittest.TestCase):
         book_37 = {'id': 37, 'title': 'The Fifth Season', 'publication_year': 2015}
         self.assertEqual(self.data_source.book(37), book_37)
 
+    def test_authors_text_jerome_lowercase(self):
+        authors_jerome = [{'id': 21, 'last_name': 'Jerome', 'first_name': 'Jerome K.','birth_year': 1859, 'death_year': 1927}]
+        self.assertEqual(self.data_source.books(search_text = "jerome"), authors_jerome)
+
+    def test_authors_text_jerome_uppercase(self):
+        authors_jerome = [{'id': 21, 'last_name': 'Jerome', 'first_name': 'Jerome K.','birth_year': 1859, 'death_year': 1927}]
+        self.assertEqual(self.data_source.books(search_text = "JEROME"), authors_jerome)
+
+    def test_authors_text_jerome_propercase(self):
+        authors_jerome = [{'id': 21, 'last_name': 'Jerome', 'first_name': 'Jerome K.','birth_year': 1859, 'death_year': 1927}]
+        self.assertEqual(self.data_source.books(search_text = "Jerome"), authors_jerome)
+
+    def test_authors_text_jerome_weirdcase(self):
+        authors_jerome = [{'id': 21, 'last_name': 'Jerome', 'first_name': 'Jerome K.','birth_year': 1859, 'death_year': 1927}]
+        self.assertEqual(self.data_source.books(search_text = "jErOmE"), authors_jerome)
+
+    def test_authors_text_nonexistent(self):
+        self.assertEqual(self.data_source.books(author_id = "Dr Pepper"), [] )
+
+    def test_books_for_author_21(self):
+        books_21 = ({'id': 40, 'title': 'Three Men in a Boat (to Say Nothing of the Dog)', 'publication_year': 1889})
+        self.assertEqual(self.data_source.books_for_author(21), books_21)
+
+    def test_books_author_id_21(self):
+        books_21 = ({'id': 40, 'title': 'Three Men in a Boat (to Say Nothing of the Dog)', 'publication_year': 1889})
+        self.assertEqual(self.data_source.books(author_id=21), books_21)
+
     #TODO test books
     #TODO test author
-    #TODO test authors
+    #TODO test authors (except search text)
     #TODO test books_for_author
     #TODO test authors_for_book
 
